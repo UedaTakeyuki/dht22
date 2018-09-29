79d2dff547cd0d2b4d3052c5375588b26c360f2b
Dr. Takeyuki Ueda <ueda@latelierdueda.com>
2018-09-29 20:09:06 +0900

    remove send feature, this responsibility move to sensorhandler

--- dht22.py
+++ dht22.py
@@ -3,7 +3,7 @@
 #
 # require: 'lol_dht' https://github.com/technion/lol_dht22
 #          
-# return:  {"temp": , "humidity":}
+# return:  {"temp": , "humidity": , "humiditydeficit":}
 
 import os
 import sys
@@ -56,13 +56,11 @@ def dht22(gpio):
 # http://d.hatena.ne.jp/Rion778/20121203/1354546179
 def HumidityDeficit(t,rh): # t: 温度, rh: 相対湿度
     ret = AbsoluteHumidity(t, 100) - AbsoluteHumidity(t, rh)
-#    print "HD = " + str(ret)
     return ret; 
 
 # http://d.hatena.ne.jp/Rion778/20121203/1354461231
 def AbsoluteHumidity(t, rh):
     ret = 2.166740 * 100 * rh * tetens(t)/(100 * (t + 273.15))
-#    print "AH = " + str(ret)
     return ret
 
 
@@ -71,7 +69,6 @@ def AbsoluteHumidity(t, rh):
 # http://d.hatena.ne.jp/Rion778/20121126/1353861179
 def tetens(t):
     ret = 6.11 * 10 ** (7.5*t/(t + 237.3))
-#    print "tetens = " + str(ret)
     return ret
 
 def read():
@@ -81,23 +78,8 @@ def read():
     result["humiditydeficit"] = ('%.1f' % HumidityDeficit(result["temp"],result["humidity"]))
     return result
 
-def send(valueid, value):
-  r = requests.post('https://monitor3.uedasoft.com/postvalue.php', data={'valueid': valueid, 'value': value}, timeout=10, verify=False)
-  print r.text
-
 if __name__ == '__main__':
 #  print json.dumps(dht22(ini.get("gpio", "gpio")))
   values = read()
-  print json.dumps(values)
-  if ini.get("valueid", "temp"):
-    print "temperature sending..."
-    send(ini.get("valueid", "temp"), values['temp'])
-  if ini.get("valueid", "humidity"):
-    print "humidity sending..."
-    send(ini.get("valueid", "humidity"), values['humidity'])
-  if ini.get("valueid", "humiditydeficit"):
-    print "humiditydeficit sending..."
-    send(ini.get("valueid", "humiditydeficit"), values['humiditydeficit'])
-
-
+  print (values)
 
